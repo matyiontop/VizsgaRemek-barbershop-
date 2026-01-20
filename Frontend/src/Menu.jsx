9d3ba7197c7dd2f38dd7366fb7bec778f1e96ea4
HeKristof <117970994+HeKristof@users.noreply.github.com>
2026-01-20 11:59:46 +0100

    menu

--- Frontend/src/Menu.jsx
+++ Frontend/src/Menu.jsx
@@ -4,24 +4,32 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
 import Register from './pages/Register';
 // import Booking from './pages/Booking';
+import Footer from './Footer';
+import './head-foot.css';
 
 function Menu() {
   return (
-    <div>
-      <nav>
-        {/* Ideiglenes menü a teszteléshez */}
-        <Link to="/">Főoldal</Link> | 
-        <Link to="/login">Bejelentkezés</Link> | 
-        <Link to="/register">Regisztráció</Link> | 
-        <Link to="/booking">Időpontfoglalás</Link>
-      </nav>
+    <div className="app-container">
+      <header>
+        <h1>Németh fodrászat</h1>
+        <nav>
+          {/* Ideiglenes menü a teszteléshez */}
+          <Link to="/">Főoldal</Link>
+          <Link to="/login">Bejelentkezés</Link> 
+          <Link to="/register">Regisztráció</Link>
+          <Link to="/booking">Időpontfoglalás</Link>
+        </nav>
+      </header>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<h1>Itt lesz a Login oldal</h1>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/booking" element={<h1>Itt lesz az Időpontfoglalás</h1>} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<h1>Itt lesz a Login oldal</h1>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/booking" element={<h1>Itt lesz az Időpontfoglalás</h1>} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ Frontend/src/footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <div>
+          <iframe 
+            width="400" 
+            height="250" 
+            src="https://maps.google.com/maps?q=Budapest&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            title="Térkép"
+          ></iframe>
+        </div>
+
+        <div>
+          <h3>Németh Fodrászat</h3>
+          <p>Cím: 1234 Budapest, Példa utca 1.</p>
+          <p>&copy; 2025 Minden jog fenntartva.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
