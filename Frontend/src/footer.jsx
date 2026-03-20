afca8d63a3124eb08762c9c94fc647001529d0d4
matyiontop <mslajko@gmail.com>
2026-03-20 10:25:12 +0100

    ADATKEZELÉSI TÁJÉKOZTATÓ

--- Frontend/src/footer.jsx
+++ Frontend/src/footer.jsx
@@ -17,6 +17,9 @@ function Footer() {
           <h3>Németh Fodrászat</h3>
           <p>Cím: Kiskunhalas, Majsai út 2, 6400</p>
           <p>&copy; 2025 Minden jog fenntartva.</p>
+          <p>
+            <a href="/adatkezelesi-tajekoztato.pdf" target="_blank" rel="noopener noreferrer">Adatkezelési Tájékoztató</a>
+          </p>
         </div>
       </div>
     </footer>
